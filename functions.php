5727007345ca18b96cf4d47b4f3cf9ed3adf6d31
Pablo Sanchez <psanchezdev345@gmail.com>
2023-08-10 09:52:50 -0600

    added block editor support

--- functions.php
+++ functions.php
@@ -27,6 +27,25 @@ function pluto_config(){
   add_theme_support( 'custom-logo' );
   add_theme_support( 'post-thumbnails' );
   add_theme_support( 'title-tag' );
+
+  // Block editor support
+  add_theme_support( 'align-wide' );
+  add_theme_support( 'responsive-embeds' );
+  add_theme_support( 'editor-styles' );
+  add_editor_style( 'style-editor.css' );
+  add_theme_support( 'wp-block-styles' );
+
+  wp_register_style( 'pluto-block-style', get_template_directory_uri() . '/block-style.css' );
+
+  register_block_style(
+    'core/quote',
+    array(
+      'name' => 'red-quote',
+      'label' => 'Red Quote',
+      'is_default' => true,
+      'style_handle' => 'pluto-block-style'
+    )
+  );
 }
 
 add_action( 'after_setup_theme', 'pluto_config' );
