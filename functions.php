1d1dc06350a90bd893b92d91acf0888902987783
Pablo Sanchez <psanchezdev345@gmail.com>
2023-08-10 10:52:40 -0600

    added theme.json file

--- functions.php
+++ functions.php
@@ -29,11 +29,23 @@ function pluto_config(){
   add_theme_support( 'title-tag' );
 
   // Block editor support
-  add_theme_support( 'align-wide' );
+  // add_theme_support( 'align-wide' );
   add_theme_support( 'responsive-embeds' );
   add_theme_support( 'editor-styles' );
   add_editor_style( 'style-editor.css' );
   add_theme_support( 'wp-block-styles' );
+  // add_theme_support( 'editor-color-palette', array(
+  //   array(
+  //     'name' => 'Primary',
+  //     'slug' => 'primary',
+  //     'color' => '#001e32'
+  //   ),
+  //   array(
+  //     'name' => 'Secondary',
+  //     'slug' => 'secondary',
+  //     'color' => '#ff1e32'
+  //   )
+  // ) );
 
   wp_register_style( 'pluto-block-style', get_template_directory_uri() . '/block-style.css' );
 
